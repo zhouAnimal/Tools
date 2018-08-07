5a6f0c7c59f9d5bc731477bbd0a766362683521a
ZXY <417901701@qq.com>
2018-08-07 10:03:26 +0800

    测试提交

--- Tools/src/main/java/tools/App.java
+++ Tools/src/main/java/tools/App.java
@@ -11,7 +11,8 @@ public class App
 //		List<String> srcFiles = ReadFile.readfile("C:\\Users\\BIGIOZ\\Desktop\\PDF");//读取某文件夹下的所有文件
 //		new ReadImgDocx().readPicture(ReadFile.readfile("C:\\Users\\BIGIOZ\\Desktop\\PDF"),"jpg");//提取docx文件内的图片
 //		Pdf2Img.pdf2Image(ReadFile.readfile("C:\\Users\\BIGIOZ\\Desktop\\PDF"),"jpg", 300);//转换pdf文件为图片
-		System.out.println("转换完成...");
+		System.out.println("转换完成了...hei");
+		
 	}
 
 }
